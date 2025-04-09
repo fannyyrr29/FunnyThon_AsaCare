2c57c72bfc78639cac56f8ea7671471f46fb8755
byMann <s160422062@student.ubaya.ac.id>
2025-04-09 13:31:16 +0700

    Admin Hospital Base

--- AsaCare/resources/views/admins/hospital.blade.php
+++ AsaCare/resources/views/admins/hospital.blade.php
@@ -15,12 +15,6 @@
                             <button type="button" class="btn btn-primary">+ Tambah</button>
                         </span>
                     </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-end">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Rumah Sakit</li>
-                        </ol>
-                    </div>
                 </div>
                 <!--end::Row-->
             </div>
@@ -34,17 +28,31 @@
                 <!--begin::Row-->
                 <div class="row">
                     <!-- Start col -->
-                    <div class="col-lg-7 connectedSortable">
+                    <div class="connectedSortable">
                         <div class="card mb-4">
                             <div class="card-header">
                                 <h3 class="card-title">Data Rumah Sakit</h3>
                             </div>
                             <div class="card-body">
-                                <table class="table table-striped">
-                                    <tr>
-                                        <th>Hospital ID</th>
-                                        <th>Count</th>
-                                    </tr>
+                                <table class="table table-striped table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>Rumah Sakit</th>
+                                            <th>Alamat</th>
+                                            <th>Nomor Telepon</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($hospitals as $hospital)
+                                            <tr>
+                                                <td>{{ $hospital->name }}</td>
+                                                <td>{{ $hospital->address }}</td>
+                                                <td>{{ $hospital->phone_number }}</td>
+                                                <td><button class="btn-primary"><i class="fa fa-trash"></i></button></td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
                                 </table>
                             </div>
                         </div>
